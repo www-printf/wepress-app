5e5c58b2fd01ab23598b7b238983143de7cf0b64
MR-Kien <nguyentrungkien130804@gmail.com>
2024-10-20 18:09:40 +0700

    landingpage + header afterlogin + add some icon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ico_printer from "../../assets/icons/ico_printer.png";
-function Header() {
+import icon_login from "../../assets/icons/icon_login.png";
+function Header({ isLoginRequired, onLoginClick }){
   return (
-    <header className="bg-blue-700 text-white p-4 flex justify-between items-center">
+    <header className="bg-blue-900 text-white p-4 flex justify-between items-center">
       <div className="flex items-center space-x-4">
         <img src={ico_printer} alt="WePress Logo" className="h-10 w-10" />
         <span className="font-bold text-xl">WePress</span>
@@ -10,11 +11,32 @@ function Header() {
           About
         </a>
       </div>
-
-      <a href="/login" className="flex items-center space-x-2">
-        <span>Log In</span>
-        <i className="fas fa-sign-in-alt"></i>
-      </a>
+      
+      {isLoginRequired ? (
+        <div className="bg-orange-500 text-white rounded-[50px] flex items-center w-[350px] h-[50px]">
+          <span className="ml-4">To use this feature, you must</span>
+          <button
+            className="login-button ml-2 flex items-center space-x-2"
+            onClick={onLoginClick} // Trigger login handler
+          >
+            <div className="bg-blue-300 text-white rounded-[50px] flex items-center w-[100px] h-[35px] m-2">
+              <span className="ml-4">Log In</span>
+              <i className="fas fa-sign-in-alt"></i>
+              <img src={icon_login} alt="icon_login" className="h-5 w-5 ml-3" />
+            </div>
+              
+          </button>
+        </div>
+      ) : (
+        <button
+          onClick={onLoginClick} // Call the handler from props
+          className=" login-button flex items-center space-x-2"
+        >
+          <span>Log In</span>
+          <i className="fas fa-sign-in-alt"></i>
+          <img src={icon_login} alt="icon_login" className="h-5 w-5" />
+        </button>
+      )}
     </header>
   );
 }
--- src/screens/Login/HomeScreen.js
+++ src/screens/Login/HomeScreen.js
@@ -1,10 +1,11 @@
 import React from "react";
 import AuthCard from "../../components/Authentication Card/Authentication";
-
-const HomeScreen = () => {
+import Landing from "../../components/Landing/Landing_unauthed";
+//import Header from "../../components/Header/Header"; 
+const HomeScreen = ({ isAuthCardVisible, setIsAuthCardVisible }) => {
   return (
-    <div className="flex-grow justify-center items-center bg-blue-300 ">
-      <AuthCard />
+    <div className="flex justify-center items-center h-full">
+      {isAuthCardVisible ? <AuthCard /> : <Landing />}
     </div>
   );
 };
