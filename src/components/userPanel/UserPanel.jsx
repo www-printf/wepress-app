b3f3a53bb00c458b5c2144132c61181c58d22b57
Ripefog <huynhngocduykhuong@gmail.com>
2024-11-12 20:27:47 +0700

    fix public/private

--- src/components/userPanel/UserPanel.jsx
+++ src/components/userPanel/UserPanel.jsx
@@ -12,40 +12,56 @@ const UserPanel = () => {
 
   useEffect(() => {
     if (!isAuthenticated) {
-      navigate(ENDPOINTS.AUTH.LOGIN); // Điều hướng về trang login nếu chưa xác thực
+      navigate(ENDPOINTS.AUTH.LOGIN, { replace: true }); // Điều hướng về trang login nếu chưa xác thực
     }
   }, [isAuthenticated, navigate]);
 
   return (
-    <div className="absolute top-16 right-0 p-[10px] text-black w-[300px] h-[220px] "> 
+    <div className="absolute top-16 right-0 p-[10px] text-black w-[300px] h-[220px] ">
       <div className="bg-white rounded-lg p-4 shadow-lg space-y-4 border-2 border-black">
         <div className="flex items-center space-x-2">
-          <img src={icon_people} alt="icon_people" className="h-10 w-10 filter invert"/>
+          <img
+            src={icon_people}
+            alt="icon_people"
+            className="h-10 w-10 filter invert"
+          />
           <span>Tên người dùng</span>
         </div>
         <div className=" border-t border-black w-full"></div>
 
         <button className="bg-white p-2 rounded-lg hover:bg-gray-100 w-full text-left flex items-center space-x-2 border-2 border-gray-200">
-          <img src={icon_list} alt="icon_list" className="h-8 w-8 filter invert"/>
+          <img
+            src={icon_list}
+            alt="icon_list"
+            className="h-8 w-8 filter invert"
+          />
           <span>Lịch sử in ấn</span>
         </button>
 
         <button className="bg-white p-2 rounded-lg hover:bg-gray-100 w-full text-left flex items-center space-x-2 border-2 border-gray-200">
-          <img src={icon_list} alt="icon_list" className="h-8 w-8 filter invert"/>
+          <img
+            src={icon_list}
+            alt="icon_list"
+            className="h-8 w-8 filter invert"
+          />
           <span>Lịch sử giao dịch</span>
         </button>
-        
+
         <div className=" border-t border-black w-full"></div>
 
         {/* Nút Đăng Xuất với xử lý logout */}
-        <button 
+        <button
           className="bg-white p-2 rounded-lg hover:bg-gray-100 w-full text-left flex justify-center items-center space-x-2 border-2 border-gray-200"
           onClick={() => {
-              logout(); 
+            logout();
           }}
         >
           <span>Đăng xuất</span>
-          <img src={icon_logout} alt="icon_logout" className="h-8 w-8 filter invert"/>
+          <img
+            src={icon_logout}
+            alt="icon_logout"
+            className="h-8 w-8 filter invert"
+          />
         </button>
       </div>
     </div>
